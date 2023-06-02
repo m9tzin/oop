d31d6972d2b8d6d3c29fea2599038eb321dca41b
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2023-06-02 16:23:13 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -3,18 +3,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		FuncionarioVendas fv = new FuncionarioVendas("Gabriel Toledoscript", "fallenfps@gmail.com", 1000.0, "Vendedor", 100.0);
+		FuncionarioVendas fv = new FuncionarioVendas("Aaaaa", "aaaaa@aaaaa.com", 1000.0, "Vendedor", 100.0);
 		fv.efetuarVenda(100);
 		System.out.println(fv.getValorTotalVendas());
 		System.out.println(fv.getSalarioBase());
 		fv.mostrarSalarioVendas();
 		System.out.println("############");
 		
-		FuncionarioAdministrativo fa = new FuncionarioAdministrativo("João Fjava", "joaofps@gmail.com", 580.0, "Auxiliar", "Monitores");
+		FuncionarioAdministrativo fa = new FuncionarioAdministrativo("Bbbbbb", "bbbbb@bbbb.com", 580.0, "Auxiliar", "Monitores");
 		System.out.println(fa.getSalarioBase());
 		fa.mostrarSalarioAdmin();
 		
-		System.out.println("------------FALLEN STORE--------------");
+		System.out.println("------------AAA STORE--------------");
 	
 		System.out.println("####### FUNCIONÁRIOS ADMINISTRATIVOS ######");
 		System.out.println(fa);
